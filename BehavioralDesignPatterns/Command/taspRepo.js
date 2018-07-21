c57f1c4acb232086e49f9387b53254341bfd0355
paulriddell <paul_riddell@outlook.com>
2018-07-21 20:08:11 +0100

    =Updating command pattern example

--- BehavioralDesignPatterns/Command/taspRepo.js
+++ BehavioralDesignPatterns/Command/taspRepo.js
@@ -1,8 +1,8 @@
 //Command Pattern on a repo
 
 var repo = {
-    tasks:[],
-    commands:[],
+    tasks:{}, //equals empty object
+    commands:[], //array of commands executed on this repo.
     select: function (id) {
         console.log('Getting task ' + id);
         return {
@@ -19,20 +19,60 @@ var repo = {
 //Execute method, takes name of method we want
 //to execute on that repo.
 repo.execute = function(name){
+    console.log('Arguments: ')
+    console.log(arguments)
+
+    //Strip first argument - which is the method name e.g save, get etc
     var args = Array.prototype.slice.call(arguments, 1);
-    
+    console.log(name);
+    console.log(repo[name]);
+
+    //adding any commands sent for execution on the repo 
+    repo.commands.push({
+        name:name,
+        obj: args[0]
+    });
+
     if(repo[name]){
-        // use 'apply' not 'call'
+        //use 'apply' not 'call'
         //apply allows passing in array of arguments,
         //whereas call you have to specify everything
+       
+        console.log(name + ' command');
         return repo[name].apply(repo, args)
     }
     if(name==='get'){
+        console.log('get command');
         return repo['select'].apply(repo, args)
     }
     return false;
 };
 
-repo.execute('save', )
 var task = repo.execute('get', 1);
-console.log(task);
+console.log(task)
+
+task = repo.execute('get', 2);
+console.log(task)
+
+repo.execute('save', {
+   id:1,
+   name: 'task 1',
+   completed: true 
+});
+repo.execute('save', {
+    id:2,
+    name: 'task 2',
+    completed: false 
+ });
+ repo.execute('save', {
+    id:3,
+    name: 'task 3',
+    completed: false
+ });
+ repo.execute('save', {
+    id:4,
+    name: 'task 4',
+    completed: false 
+ });
+console.log(repo.tasks)
+console.log(repo.commands)
